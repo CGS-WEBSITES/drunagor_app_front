c92812bc012f47f45fb855066e3494197ccf175f
Alicia Delgado <aliciajf1215@yahoo.com.br>
2024-10-30 15:11:41 -0300

    Commitando tela de login figma

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app theme="light">
+    <v-app-bar flat class="border-b" title="App Drunagor">
+      <!-- Links para navegação -->
+      <v-spacer></v-spacer>
+      <v-btn to="/login">Login</v-btn>
+      <v-btn to="/dashboard">Dashboard</v-btn>
+      <v-btn to="/teste">teste</v-btn>
+      <v-btn to="/testedash">testedash</v-btn>
+
+    </v-app-bar>
+
+    <v-main>
+      <!-- Exibe o conteúdo da rota -->
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
