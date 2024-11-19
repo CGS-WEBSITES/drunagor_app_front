2c1ec559c4d25a9f9f3a26a8d4265f4ce1a5d9dc
Thiago Esterci Fernandes <thiago.esterci@engenharia.ufjf.br>
2024-11-19 08:49:39 -0300

    Update CampaignLog.vue

--- src/components/CampaignLog.vue
+++ src/components/CampaignLog.vue
@@ -23,57 +23,68 @@ const hero = heroDataRepository.find(props.heroId) ?? ({} as HeroData);
 </script>
 
 <template>
-  <v-card elevation="16" rounded style="background-color: #1f2937" width="100%">
-    <v-card-title class="text-h5">
+  <v-card elevation="16" rounded style="background-color: #1f2937" width="100%" class="hero-list-item">
+    <v-card-title class="text-h5 px-2">
       {{ hero.name }}
       <v-divider></v-divider>
     </v-card-title>
-    <v-card-text class="hero-list-item" >
+    <v-card-text class="px-2">
       <v-row no-gutters>
-        <v-col cols="2" >
-          <v-avatar
-            :image="hero.images.avatar"
-            size="65"
-            class="ml-2"
-          />
+        <v-col cols="2">
+          <v-avatar :image="hero.images.avatar" size="65" />
         </v-col>
         <v-col cols="10">
           <p>
             {{ t("label." + hero.race.toLowerCase()) }}
             {{ t("label." + hero.class.toLowerCase().replace(" ", "-")) }}
           </p>
-          <p>{{ t("text.path-of") }} {{ t("label." + hero.path.toLowerCase()) }}</p>
+          <p>
+            {{ t("text.path-of") }} {{ t("label." + hero.path.toLowerCase()) }}
+          </p>
         </v-col>
       </v-row>
     </v-card-text>
     <v-card-actions>
-      <CampaignLogSequentialAdventure
-        v-if="isSequentialAdventure"
-        :hero="hero"
-        :campaign-id="campaignId"
-      />
-      <CampaignLogCore
-        v-if="campaign.campaign == 'core'"
-        :campaign-id="props.campaignId"
-        :hero-id="props.heroId"
-      />
-      <CampaignLogAwakenings
-        v-if="campaign.campaign == 'awakenings'"
-        :campaign-id="props.campaignId"
-        :hero-id="props.heroId"
-      />
-      <CampaignLogApocalypse
-        v-if="campaign.campaign == 'apocalypse'"
-        :campaign-id="props.campaignId"
-        :hero-id="props.heroId"
-      />
+      <v-row no-gutters>
+        <v-col cols="12">
+          <CampaignLogSequentialAdventure
+            v-if="isSequentialAdventure"
+            :hero="hero"
+            :campaign-id="campaignId"
+          />
+        </v-col>
+
+        <v-col cols="12">
+          <CampaignLogCore
+            v-if="campaign.campaign == 'core'"
+            :campaign-id="props.campaignId"
+            :hero-id="props.heroId"
+          />
+        </v-col>
+
+        <v-col cols="12">
+          <CampaignLogAwakenings
+            v-if="campaign.campaign == 'awakenings'"
+            :campaign-id="props.campaignId"
+            :hero-id="props.heroId"
+          />
+        </v-col>
+
+        <v-col cols="12">
+          <CampaignLogApocalypse
+            v-if="campaign.campaign == 'apocalypse'"
+            :campaign-id="props.campaignId"
+            :hero-id="props.heroId"
+          />
+        </v-col>
+      </v-row>
     </v-card-actions>
   </v-card>
 </template>
 
 <style scoped>
 .hero-list-item {
-  padding: 16px;
+  padding: 0px 16px 16px 16px;
   background-image: url("@/assets/hero/flag-bg-red.webp");
   background-repeat: no-repeat;
   background-origin: content-box;
