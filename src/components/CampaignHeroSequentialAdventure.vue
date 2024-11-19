41785dc53b6edbd02474430a70e3b08690fd2253
Thiago Esterci Fernandes <thiago.esterci@engenharia.ufjf.br>
2024-11-19 08:49:38 -0300

    Update CampaignHeroSequentialAdventure.vue

--- src/components/CampaignHeroSequentialAdventure.vue
+++ src/components/CampaignHeroSequentialAdventure.vue
@@ -6,6 +6,7 @@ import { HeroDataRepository } from "@/data/repository/HeroDataRepository";
 import { useRoute } from "vue-router";
 import type { HeroData } from "@/data/repository/HeroData";
 import { useI18n } from "vue-i18n";
+import { VNumberInput } from "vuetify/labs/VNumberInput";
 
 const resourceDefinitions = [
   { id: "focus", translation_key: "label.focus" },
@@ -28,7 +29,7 @@ const sequentialAdventureState = ref({} as SequentialAdventureState);
 
 const currentSeqAdv = heroStore.findInCampaign(
   heroId,
-  campaignId,
+  campaignId
 ).sequentialAdventureState;
 const seqAdvState =
   typeof currentSeqAdv !== "undefined" && currentSeqAdv !== null
@@ -43,101 +44,103 @@ watch(
     heroStore.findInCampaign(heroId, campaignId).sequentialAdventureState =
       newState;
   },
-  { deep: true },
+  { deep: true }
 );
 </script>
 
 <template>
-  <div>
-    <Button outlined label="Back" @click="$router.go(-1)"></Button>
-  </div>
-  <div
-    id="hero-card"
-    class="bg-neutral form-control drop-shadow rounded-lg p-4 mt-4"
-    style="background-color: #1f2937"
-  >
-    <div class="flex h-28">
-      <div>
-        <img
-          class="-ml-1 w-14 rounded-full hero-image"
-          :src="hero.images.avatar"
-        />
-      </div>
-      <div class="pl-8">
-        <p>{{ hero.name }}</p>
-        <p>
-          {{ t("label." + hero.race.toLowerCase()) }}
-          {{ t("label." + hero.class.toLowerCase().replace(" ", "-")) }}
-        </p>
-        <p>
-          {{ t("text.path-of") }} {{ t("label." + hero.path.toLowerCase()) }}
-        </p>
-      </div>
-    </div>
+  <v-row no-gutters>
+    <v-col cols="12" class="py-6">
+      <v-btn variant="outlined" @click="$router.go(-1)">Back</v-btn>
+    </v-col>
 
-    <div id="sequential-select" class="pt-4">
-      <label for="curse-cubes" class="block pt-4">{{
-        t("text.curse-cubes")
-      }}</label>
-      <InputNumber
-        :inputStyle="{ width: '100%' }"
-        incrementButtonIcon="pi pi-plus"
-        decrementButtonIcon="pi pi-minus"
-        id="curse-cubes"
-        name="curse-cubes"
-        showButtons
-        buttonLayout="horizontal"
-        :min="0"
-        :max="5"
-        v-model="sequentialAdventureState.curseCubes"
-      />
-
-      <label for="trauma-cube" class="block py-2">{{
-        t("text.trauma-cubes")
-      }}</label>
-      <InputNumber
-        :inputStyle="{ width: '100%' }"
-        incrementButtonIcon="pi pi-plus"
-        decrementButtonIcon="pi pi-minus"
-        id="trauma-cube"
-        name="trauma-cube"
-        showButtons
-        buttonLayout="horizontal"
-        :min="0"
-        :max="1"
-        v-model="sequentialAdventureState.traumaCubes"
-      />
-    </div>
-
-    <Divider>{{ t("label.resources") }}</Divider>
-
-    <div id="resource-select">
-      <template v-for="resource in resourceDefinitions" :key="resource.id">
-        <label :for="resource.id" class="block py-2">{{
-          t(resource.translation_key)
-        }}</label>
-        <InputNumber
-          :inputStyle="{ width: '100%' }"
-          incrementButtonIcon="pi pi-plus"
-          decrementButtonIcon="pi pi-minus"
-          :name="resource.id"
-          :id="resource.id"
-          showButtons
-          buttonLayout="horizontal"
-          :min="0"
-          :max="4"
-          v-model.number="
-            sequentialAdventureState.resources[resource.translation_key]
-          "
-        />
-      </template>
-    </div>
-  </div>
+    <v-col cols="12">
+      <v-card
+        elevation="16"
+        rounded
+        style="background-color: #1f2937"
+        width="100%"
+        class="hero-list-item"
+      >
+        <v-card-title class="text-h5 px-2">
+          {{ hero.name }}
+          <v-divider></v-divider>
+        </v-card-title>
+        <v-card-text class="px-2">
+          <v-row no-gutters>
+            <v-col cols="2">
+              <v-avatar :image="hero.images.avatar" size="65" />
+            </v-col>
+            <v-col cols="10">
+              <p>
+                {{ t("label." + hero.race.toLowerCase()) }}
+                {{ t("label." + hero.class.toLowerCase().replace(" ", "-")) }}
+              </p>
+              <p>
+                {{ t("text.path-of") }}
+                {{ t("label." + hero.path.toLowerCase()) }}
+              </p>
+            </v-col>
+            <v-col cols="12" class="py-6">
+              <v-number-input
+                :reverse="false"
+                controlVariant="split"
+                :label="t('text.curse-cubes')"
+                :hideInput="false"
+                :inset="false"
+                variant="outlined"
+                id="curse-cubes"
+                :min="0"
+                :max="5"
+                v-model="sequentialAdventureState.curseCubes"
+              ></v-number-input>
+            </v-col>
+            <v-col cols="12" class="py-6">
+              <v-number-input
+                :reverse="false"
+                controlVariant="split"
+                :label="t('text.trauma-cubes')"
+                :hideInput="false"
+                :inset="false"
+                variant="outlined"
+                id="trauma-cubes"
+                :min="0"
+                :max="1"
+                v-model="sequentialAdventureState.traumaCubes"
+              ></v-number-input>
+            </v-col>
+            <v-divider></v-divider>
+            <v-col cols="12" class="py-6 text-center text-h6">{{
+              t("label.resources")
+            }}</v-col>
+            <v-col cols="12"
+              ><v-number-input
+                v-for="resource in resourceDefinitions"
+                :key="resource.id"
+                :reverse="false"
+                controlVariant="split"
+                :label="t(resource.translation_key)"
+                :hideInput="false"
+                :inset="false"
+                variant="outlined"
+                id="resource.id"
+                :min="0"
+                :max="4"
+                v-model="sequentialAdventureState.resources[resource.translation_key]"
+              ></v-number-input
+            ></v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <style scoped>
-#hero-card {
+.hero-list-item {
+  padding: 0px 16px 16px 16px;
   background-image: url("@/assets/hero/flag-bg-red.webp");
   background-repeat: no-repeat;
+  background-origin: content-box;
 }
 </style>
