79104eb15f123efec39eb97efc09abdc140686fd
Thiago Esterci Fernandes <thiago.esterci@engenharia.ufjf.br>
2024-11-23 11:57:28 -0300

    Update HeroDetailView.vue

--- src/components/HeroDetailView.vue
+++ src/components/HeroDetailView.vue
@@ -42,14 +42,84 @@ function onStash() {
 </script>
 
 <template>
-  <div>
-    <Button
-      outlined
-      id="campaign-export"
-      :label="t('label.back')"
-      @click="$router.go(-1)"
-    ></Button>
-  </div>
+  <v-row no-gutters
+    ><v-col cols="12">
+      <v-btn variant="outlined" @click="$router.go(-1)">{{
+        t("label.back")
+      }}</v-btn>
+    </v-col></v-row
+  >
+  <v-row no-gutters
+    ><v-col cols="12">
+      <v-card
+        elevation="16"
+        rounded
+        style="background-color: #1f2937"
+        width="100%"
+        class="hero-list-item"
+      >
+        <v-card-title class="text-h5 px-2">
+          {{ hero.name }}
+          <v-divider></v-divider>
+        </v-card-title>
+        <v-card-text class="px-2">
+          <v-row no-gutters>
+            <v-col cols="2">
+              <v-avatar :image="hero.images.avatar" size="65" />
+            </v-col>
+            <v-col cols="10">
+              <p>
+                {{ t("label." + hero.race.toLowerCase()) }}
+                {{ t("label." + hero.class.toLowerCase().replace(" ", "-")) }}
+              </p>
+              <p>
+                {{ t("text.path-of") }}
+                {{ t("label." + hero.path.toLowerCase()) }}
+              </p>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-row no-gutters
+            ><v-col cols="12">
+              <v-divider></v-divider>
+
+              <div class="text-center text-h5">
+                {{ t("label.equipment") }}
+              </div>
+              <CampaignHeroItems
+                :campaign-id="campaignId"
+                :hero-id="heroId"
+                :repository="repository"
+                :hero="hero"
+                @stash="onStash"
+              /> </v-col
+            ><v-col cols="12">
+              <v-divider></v-divider>
+
+              <div class="text-center text-h5">
+                {{ t("label.stash") }}
+              </div>
+              <CampaignHeroStash
+                :campaign-id="campaignId"
+                :repository="repository"
+                :hero-id="heroId"
+                :key="stash"
+              /> </v-col
+            ><v-col cols="12">
+              <v-divider></v-divider>
+
+              <div class="text-center text-h5">
+                {{ t("label.skills") }}
+              </div>
+              <CampaignHeroSkills
+                :campaign-id="campaignId"
+                :hero-id="heroId"
+              ></CampaignHeroSkills> </v-col
+          ></v-row>
+        </v-card-actions>
+      </v-card> </v-col
+  ></v-row>
   <div
     id="hero-card"
     class="bg-neutral form-control drop-shadow rounded-lg p-4 mt-4"
@@ -75,36 +145,6 @@ function onStash() {
     </div>
 
     <Divider>{{ t("label.equipment") }}</Divider>
-
-    <div class="py-2 w-full">
-      <CampaignHeroItems
-        :campaign-id="campaignId"
-        :hero-id="heroId"
-        :repository="repository"
-        :hero="hero"
-        @stash="onStash"
-      />
-    </div>
-
-    <Divider>{{ t("label.stash") }}</Divider>
-
-    <div class="hero-stash-wrapper py-2 w-full">
-      <CampaignHeroStash
-        :campaign-id="campaignId"
-        :repository="repository"
-        :hero-id="heroId"
-        :key="stash"
-      />
-    </div>
-
-    <Divider>{{ t("label.skills") }}</Divider>
-
-    <div class="py-2 w-full">
-      <CampaignHeroSkills
-        :campaign-id="campaignId"
-        :hero-id="heroId"
-      ></CampaignHeroSkills>
-    </div>
   </div>
 </template>
 
