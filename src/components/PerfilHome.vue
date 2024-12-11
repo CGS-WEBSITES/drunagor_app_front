544056473a4b4f2c2d6e0bb7304614112deeff4f
dudu1337 <54040715+dudu1337@users.noreply.github.com>
2024-12-11 15:44:09 -0300

    Ajustando e mudando imagens no perfilhome

--- src/components/PerfilHome.vue
+++ src/components/PerfilHome.vue
@@ -3,76 +3,14 @@
     <v-row no-gutters>
         <v-col cols="12">
             <statistics-card title="STATISTICS" :stats="statsData" user-since="11/11/2011" />
-            <achievements-list title="ACHIEVEMENTS" total="[TOTAL]" :achievements="achievementsData" />
+            <achievements-list title="ACHIEVEMENTS" total="13/37" :achievements="achievementsData" />
             <FavoriteCampaignCard
-                image="https://lh6.googleusercontent.com/proxy/2qMKbpv3vomFIt5hI_Rxy6fuNTDBuQMR7IC6wx-4fWYguoWKNXz27FwuwLmNS-iMQa8CqHyPEDrcWIyW8WikWY8gd20NVuoMCgv7uxST8nxBbNq5z0YZYs7mzhStq_Q"
+                image="https://i.ibb.co/bJzLDg8/21312312313123.png"
                 title="FAVORITE CAMPAIGN"
-                subtitle="Chronicles of Drunagor: Age of Darkness The Rise of the Undead Dragon" progress="100"
+                subtitle="CoD: Age of Darkness"
+                subtitle2="The Rise of the Undead Dragon" 
                 details="Door 01 - The entrance to the citadel - Chapter 2" />
-            <badges-list title="BADGES" total="[TOTAL]" :badges="badgesData" />
-        </v-col>
-        <v-col cols="12">
-            <v-card  max-width="500">
-                <v-row no-gutters>
-                    <v-col cols="6" id="navigation">
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-
-                        </p>
-                    </v-col>
-                    <v-col cols="6" id="navigation2">
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-
-                        </p>
-                        <p>
-                            dsfjlbhsdkljfhakfjhfsdkjsfhdlk
-
-                        </p>
-                    </v-col>
-                </v-row>
-
-            </v-card>
+            <badges-list title="BADGES" total="13/37" :badges="badgesData" />
         </v-col>
     </v-row>
 
@@ -106,7 +44,7 @@ import AchievementsList from '@/components/AchievementsList.vue';
 // Dados das conquistas
 const achievementsData = [
     {
-        image: 'https://cdn-icons-png.flaticon.com/512/1312/1312197.png', // Substituir pelo caminho da imagem
+        image: 'https://i.ibb.co/9cBwVj9/Rectangle12213121.png',
         title: 'GREAT ACHIEVEMENT',
         description: 'Achievement short description here cabe duas',
         date: '11/11/2024',
@@ -114,7 +52,7 @@ const achievementsData = [
         progress: '15/43',
     },
     {
-        image: 'https://cdn-icons-png.flaticon.com/512/1312/1312197.png',
+        image: 'https://i.ibb.co/9cBwVj9/Rectangle12213121.png',
         title: 'GREAT ACHIEVEMENT',
         description: 'Achievement short description here cabe duas',
         date: '11/11/2024',
@@ -122,7 +60,7 @@ const achievementsData = [
         progress: '15/43',
     },
     {
-        image: 'https://cdn-icons-png.flaticon.com/512/1312/1312197.png',
+        image: 'https://i.ibb.co/9cBwVj9/Rectangle12213121.png',
         title: 'GREAT ACHIEVEMENT',
         description: 'Achievement short description here cabe duas',
         date: '11/11/2024',
@@ -136,7 +74,7 @@ import badgesList from '@/components/Badges.vue';
 
 const badgesData = [
     {
-        image: 'https://cdn.mos.cms.futurecdn.net/P9nEHqSzs3dUrSdmBDrPxn.jpg', // Substituir pelo caminho da imagem
+        image: 'https://i.ibb.co/8MQPSjg/Rectangle121.webp', // Substituir pelo caminho da imagem
         title: 'BADGE FODA 1',
         description: 'MT MIDIAAAAA',
         date: '11/11/2024',
@@ -144,7 +82,7 @@ const badgesData = [
         progress: '15/43',
     },
     {
-        image: 'https://cdn.mos.cms.futurecdn.net/P9nEHqSzs3dUrSdmBDrPxn.jpg',
+        image: 'https://i.ibb.co/8MQPSjg/Rectangle121.webp',
         title: 'BADGE FODA 2',
         description: 'MT MIDAAAA 2',
         date: '11/11/2024',
@@ -152,7 +90,7 @@ const badgesData = [
         progress: '15/43',
     },
     {
-        image: 'https://i.pinimg.com/736x/41/41/6a/41416adf9a5754774ed8798406808dfd.jpg',
+        image: 'https://i.ibb.co/8MQPSjg/Rectangle121.webp',
         title: 'BADGE FODA 3',
         description: 'MT MIDIAAAAA 3',
         date: '11/11/2024',
@@ -165,17 +103,4 @@ const badgesData = [
 </script>
 
 <style scoped>
-#navigation {
-    background-image: url("https://cdn-icons-png.flaticon.com/512/1312/1312197.png");
-    background-color: rgba(255, 255, 255, 0.6);
-    background-blend-mode: darken;
-}
-
-#navigation2 {
-    background-image: url("https://i.pinimg.com/736x/41/41/6a/41416adf9a5754774ed8798406808dfd.jpg");
-    background-color: rgba(255, 255, 255, 0.6);
-    background-blend-mode: darken;
-}
-
-
 </style>
