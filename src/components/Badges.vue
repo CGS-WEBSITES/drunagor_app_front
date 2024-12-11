cef9d3f17f0288aed5649ed11bbc23a3887b82cf
dudu1337 <54040715+dudu1337@users.noreply.github.com>
2024-12-11 15:40:13 -0300

    Mudanças no badges
    Ajustando tamanhos e posiçoes, isso é no perfil home

--- src/components/Badges.vue
+++ src/components/Badges.vue
@@ -1,42 +1,42 @@
 <template>
     <v-container max-width="800" style="min-width: 360px;" class="pa-4">
-      <v-card rounded="lg" elevation="3" class="pa-5">
+      <v-card rounded="lg" elevation="3" class="pl-1 pt-1 pr-1 pb-0">
         <!-- Cabeçalho -->
         <v-card-title class="d-flex justify-space-between">
-          <span class="text-h6 text-uppercase text-bold">{{ title }}</span>
-          <span class="text-body-2 grey--text text-bold">{{ total }}</span>
+          <span class="text-uppercase font-weight-black text-bold text-h5 mb-4 pb-0">{{ title }}</span>
+          <span class="text-uppercase text-bold text-h5 mb-4 pb-0">{{ total }}</span>
         </v-card-title>
   
         <!-- Virtual Scroller -->
-        <v-virtual-scroll :items="badges" :item-height="130">
+        <v-virtual-scroll class="pt-0":items="badges" :item-height="130">
           <template #default="{ item }">
             <v-card
               rounded="lg"
               elevation="3"
-              class="pa-3 mb-2 d-flex align-center"
-              style="background-color: #484848;"
+              class="pa-0 mb-1 d-flex align-center"
+              style="background-color: darkslategray;"
             >
               <v-row class="align-center">
                 <!-- Ícone -->
-                <v-col cols="3" class="d-flex align-center justify-center">
+                <v-col cols="4" lg="2" class="d-flex align-center justify-center">
                   <v-img
                     :src="item.image"
                     alt="Achievement Icon"
-                    max-width="70"
-                    max-height="70"
+                    max-width="80"
+                    max-height="80"
                     class="rounded-lg"
                   ></v-img>
                 </v-col>
   
                 <!-- Detalhes -->
-                <v-col cols="7">
-                  <p class="text-body-1 text-bold white--text">{{ item.title }}</p>
-                  <p class="text-body-2 grey--text">{{ item.description }}</p>
-                  <p class="text-caption grey--text">Achieved {{ item.date }}</p>
+                <v-col cols="7" class="pl-0 d-flex flex-column justify-center">
+                  <p class="font-weight-bold white--text ma-0">{{ item.title }}</p>
+                  <p class="text-body-2 grey--text ma-0">{{ item.description }}</p>
+                  <p class="text-caption grey--text ma-0">Achieved {{ item.date }}</p>
                 </v-col>
   
                 <!-- Status -->
-                <v-col cols="1" class="d-flex flex-column align-center justify-center">
+                <v-col cols="1" lg="3" class="d-flex flex-column align-center justify-center">
                   <v-icon color="grey darken-1" size="24">{{ item.icon }}</v-icon>
                   <p class="text-body-2 grey--text">{{ item.progress }}</p>
                 </v-col>
