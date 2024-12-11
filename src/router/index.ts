330fe39cbc6730d3cc656e384c4977accc11fb0c
Alicia Delgado <aliciajf1215@yahoo.com.br>
2024-12-11 14:56:34 -0300

    Retirando pagina "teste"
    Login antigo que não vai ser usado

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Login from "@/pages/teste.vue";
-import Teste from "@/pages/Login.vue";
+import Login from "@/pages/Login.vue";
 import Dashboard from "@/pages/Dashboard.vue"; // Descomentei a importação do Dashboard
 import Library from "@/pages/Library.vue";
-import Perfil from "@/pages/Perfil.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,11 +11,7 @@ const router = createRouter({
       name: "Login",
       component: Login,
     },
-    {
-      path: "/teste", // Defina a rota para o arquivo Teste.vue
-      name: "Teste",
-      component: Teste,
-    },
+  
     {
       path: "/dashboard", // Defina a rota para o arquivo Dashboard.vue
       name: "Dashboard",
