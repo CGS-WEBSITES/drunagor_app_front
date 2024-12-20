3ac0f7f79cd1f4affaf9b6fb3602a385110a5192
dudu1337 <54040715+dudu1337@users.noreply.github.com>
2024-12-20 16:21:43 -0300

    Adjusting Navegation Bar
    diferentation between pc and mobile

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container max-width="720" style="min-width: 360px;" class="pa-4">
+  <v-container max-width="800" style="min-width: 360px;" class="d-md-none pa-4">
     <v-card rounded="lg" elevation="3" class="mx-auto py-4 px-6 d-flex justify-center">
-      <v-row justify="space-between" align="center" class="py-2" style="max-width: 400px;">
+      <v-row justify="space-between" align="center" class="py-2" style="max-width: 800px;">
         <!-- Grupo 1: Profile e Edit -->
         <template v-for="(button,) in group1" :key="button.value">
           <v-btn
@@ -29,6 +29,17 @@
           </v-btn>
         </template>
 
+        <template v-for="(button,) in group25" :key="button.value">
+          <v-btn
+            icon
+            size="45"
+            :class="{ 'text-primary': activeButton === button.value }"
+            @click="navigateTo(button.route)"
+          >
+            <v-icon style="font-size: 24px;">{{ button.icon }}</v-icon>
+          </v-btn>
+        </template>
+
         <!-- Separador -->
         <v-divider vertical class="mx-2"></v-divider>
 
@@ -43,10 +54,97 @@
             <v-icon style="font-size: 24px;">{{ button.icon }}</v-icon>
           </v-btn>
         </template>
+
+        <template v-for="(button,) in group4" :key="button.value">
+          <v-btn
+            icon
+            size="45"
+            :class="{ 'text-primary': activeButton === button.value }"
+            @click="navigateTo(button.route)"
+          >
+            <v-icon style="font-size: 24px;">{{ button.icon }}</v-icon>
+          </v-btn>
+        </template>
+
+      </v-row>
+    </v-card>
+  </v-container>
+
+  <v-container max-width="800" style="min-width: 360px;" class="d-none d-md-flex">
+    <v-card rounded="lg" elevation="3" class="mx-auto py-4 px-6 d-flex justify-center ">
+      <v-row justify="space-between" align="center" class="py-2" style="max-width: 800px;">
+        <!-- Grupo 1: Profile e Edit -->
+        <template v-for="(button,) in group1" :key="button.value">
+          <v-btn
+          rounded
+           class=".d-none .d-md-flex .d-lg-none"
+          :class="{ 'text-primary': activeButton === button.value }"
+           @click="navigateTo(button.route)"
+            >
+            <v-icon style="font-size: 24px;">{{ button.icon }}</v-icon>
+            <span class="ml-0 d-none d-md-inline"> PROFILE </span>
+           </v-btn>
+        </template>
+
+
+        <!-- Grupo 2: Search e Group -->
+        <template v-for="(button,) in group2" :key="button.value">
+          <v-btn
+          rounded
+           class="d-flex align-center "
+          :class="{ 'text-primary': activeButton === button.value }"
+           @click="navigateTo(button.route)"
+            >
+            <v-icon style="font-size: 24px;">{{ button.icon }}</v-icon>
+            <span class="ml-1 d-none d-md-inline">SEARCH USERS</span>
+           </v-btn>
+        </template>
+
+        <template v-for="(button,) in group25" :key="button.value">
+          <v-btn
+          rounded
+           class="d-flex align-center pa-3"
+          :class="{ 'text-primary': activeButton === button.value }"
+           @click="navigateTo(button.route)"
+            >
+            <v-icon style="font-size: 24px;">{{ button.icon }}</v-icon>
+            <span class="ml-1 d-none d-md-inline">FRIENDS</span>
+           </v-btn>
+        </template>
+
+
+
+        <!-- Grupo 3: Settings e Logout -->
+        <template v-for="(button,) in group3" :key="button.value">
+          <v-btn
+          rounded
+           class="d-flex align-center pa-3"
+          :class="{ 'text-primary': activeButton === button.value }"
+           @click="navigateTo(button.route)"
+            >
+            <v-icon style="font-size: 24px;">{{ button.icon }}</v-icon>
+            <span class="ml-1 d-none d-md-inline">USER CONFIG</span>
+           </v-btn>
+        </template>
+
+        <template v-for="(button,) in group4" :key="button.value">
+          <v-btn
+          rounded
+           class="d-flex align-center pa-3"
+          :class="{ 'text-primary': activeButton === button.value }"
+           @click="navigateTo(button.route)"
+            >
+            <v-icon style="font-size: 24px;">{{ button.icon }}</v-icon>
+            <span class="ml-2 d-none d-md-inline">LOGOUT</span>
+           </v-btn>
+        </template>
+
       </v-row>
     </v-card>
   </v-container>
 </template>
+
+
 
 <script lang="ts" setup>
 import { ref } from 'vue';
@@ -71,11 +169,17 @@ const group1 = ref([
 
 const group2 = ref([
   { icon: 'mdi-magnify', value: 'search', route: '/perfil/friend-store' },
+]);
+
+const group25 = ref([
   { icon: 'mdi-account-group', value: 'group', route: '/perfil/friend-storelist' },
 ]);
 
 const group3 = ref([
   { icon: 'mdi-cog-outline', value: 'settings', route: '/perfil/perfil-settings' },
-  { icon: 'mdi-logout', value: 'logout', route: '/Perfil' },
+]);
+
+const group4 = ref([
+{ icon: 'mdi-logout', value: 'logout', route: '/Perfil' },
 ]);
 </script>
