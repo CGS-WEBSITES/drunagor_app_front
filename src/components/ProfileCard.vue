f28e70d1e7da66661475d417b7adc028cdac3fe2
dudu1337 <54040715+dudu1337@users.noreply.github.com>
2024-12-26 15:46:09 -0300

    Adjust top perfil card

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,44 +1,21 @@
 <template>
-  <v-container style="min-width: 360px;" class="pa-0 ">
-  <v-card
-    class="profile-card mx-auto py-0"
-    rounded="0"
-    elevation="3"
-    
+  <v-card class="profile-card mx-auto py-0" rounded="0" elevation="3"
+    style="overflow: visible; position: relative; text-align: center; width: 100%;">
+    <div class="position-relative">
+      <v-img src="/home/dudu/Documentos/app-drunagor-front/src/assets/AGE_Arte271.png" alt="Background Image"
+        max-height="729px" max-width="100%" cover></v-img>
 
-    style=";overflow: visible; position: relative; text-align: center;"
-  >
-  <div class="position-relative">
-      <v-img
-        src="/home/dudu/Documentos/app-drunagor-front/src/assets/AGE_Arte271.png"
-        alt="Background Image"
-        max-height="729px"
-        max-width="100%"
-        cover
-        
-      ></v-img>
-
-      <v-btn
-  icon="mdi-pencil"
-  class="position-absolute top-0 right-0 ma-2"
-  color="rgba(0, 0, 0, 0.6)"
-  elevation="3"
-  :to="'/perfil/perfil-image'"
-></v-btn>
+      <v-btn icon="mdi-pencil" class="position-absolute top-0 right-0 ma-2" color="rgba(0, 0, 0, 0.6)" elevation="3"
+        :to="'/perfil/perfil-image'"></v-btn>
     </div>
-    
-    <v-img
-      :src="profileImage"
-      :alt="altText"
-      max-width="118"
-      style="
+
+    <v-img :src="profileImage" :alt="altText" max-width="118" style="
         top: -30px;
         left: 50%;
         transform: translate(-50%, -50%);
         border: 0.5px solid white;
         box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
-      "
-    ></v-img>
+      "></v-img>
 
 
     <v-card-text>
@@ -55,8 +32,6 @@
       </div>
     </v-card-text>
   </v-card>
-</v-container>
-
 
 </template>
 
