20349eb394bd53c466cdb6c0e425fddaf06d65b5
Thiago Esterci Fernandes <estercifernandes@gmail.com>
2025-01-02 15:42:48 -0300

    Revert "Direcionando dados para usuarios"
    This reverts commit a8576c9128bcb8d4d96714926d10f34d4665256d.

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,17 +1,8 @@
-// store/app.ts
+// Utilities
 import { defineStore } from "pinia";
 
 export const useAppStore = defineStore("app", {
   state: () => ({
-    user: {
-      username: "", // Nome de usuário
-      email: "",    // Email do usuário (opcional)
-    },
+    //
   }),
-  actions: {
-    setUser(userData: { username: string; email?: string }) {
-      this.user.username = userData.username;
-      if (userData.email) this.user.email = userData.email;
-    },
-  },
 });
