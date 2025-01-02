2ab1d0ab07ecf14e76b2a2501505f42838444623
dudu1337 <54040715+dudu1337@users.noreply.github.com>
2025-01-02 16:30:55 -0300

    Adjusting routes on bottom and lateral navegation

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,37 @@
 <template>
-<v-app :theme="theme">
-  <Toast />
+  <v-app :theme="theme">
+    <Toast />
 
-    
+
     <!-- Barra de Navegação Superior -->
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      location="right"
-      temporary
-      class="d-none d-md-flex m"
-    >
+    <v-navigation-drawer v-model="drawer" app location="right" temporary class="d-none d-md-flex m">
       <v-list class="me-4">
-     
+
         <v-list-item class="py-5">
-       
+
           <v-row align="center" class="">
-            
+
             <v-col cols="8">
               <v-list-item-title>Magoveio92magi</v-list-item-title>
               <v-list-item-subtitle>Points: 1337</v-list-item-subtitle>
             </v-col>
 
 
-     
+
 
 
             <!-- Coluna para o avatar à direita -->
             <v-col cols="4" class="d-flex justify-end">
               <v-avatar size="100">
-                <v-img
-                  src="https://segredoquantico.com/wp-content/uploads/2023/07/o-arquetipo-do-mago.webp"
-                  alt="Avatar"
-                />
+                <v-img src="https://segredoquantico.com/wp-content/uploads/2023/07/o-arquetipo-do-mago.webp"
+                  alt="Avatar" />
               </v-avatar>
             </v-col>
           </v-row>
         </v-list-item>
 
-        <v-list-item
-          v-for="(item, index) in menuItems"
-          :key="index"
-          link
-          @click="selectItem(item)"
-          :class="{ 'v-list-item--active': selectedItem === item }"
-        >
+        <v-list-item v-for="(item, index) in menuItems" :key="index" link @click="router.push(item.to)"
+          :class="{ 'v-list-item--active': selectedItem === item }">
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -58,74 +45,35 @@
       <v-app-bar app min-height="50" class="hidden-md-and-down" color="black">
         <div class="d-flex align-center pl-6">
           <!-- Ajuste o padding com pl-6 -->
-          <v-img
-            src="@/assets/darknessl.png"
-            height="30"
-            width="30"
-            alt="Drunagor Icon"
-            contain
-            class="mr-2"
-          ></v-img>
+          <v-img src="@/assets/darknessl.png" height="30" width="30" alt="Drunagor Icon" contain class="mr-2"></v-img>
           <span>App Drunagor</span>
         </div>
         <v-spacer></v-spacer>
         <!-- Botão de Navegação alinhado à direita -->
-        <v-app-bar-nav-icon
-          class="me-4"
-          @click="drawer = !drawer"
-        ></v-app-bar-nav-icon>
-        </v-app-bar>
+        <v-app-bar-nav-icon class="me-4" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      </v-app-bar>
+    </v-row>
+
+    <v-bottom-navigation app v-model="bottomNavVisible" class="hidden-md-and-up fixed bottom-0 bg-black text-white"
+      elevation="10" dense>
+      <v-row align="center" justify="space-between" no-gutters>
+        <v-col v-for="(item, index) in menuItems" :key="index" link @click="router.push(item.to)":class="{ 'v-list-item--active': selectedItem === item }" cols="2">
+          <v-btn @click="router.push(to)" icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-btn>
+
+        </v-col>
       </v-row>
+    </v-bottom-navigation>
 
-      <v-bottom-navigation
-        app
-        v-model="bottomNavVisible"
-        class="hidden-md-and-up fixed bottom-0 bg-black text-white"
-        elevation="10"
-        dense
-      >
-        <v-row align="center" justify="space-between" no-gutters>
-          <v-col class="text-center" cols="2">
-            <v-btn @click="action1" icon>
-              <v-icon>mdi-home</v-icon>
-            </v-btn>
-          </v-col>
-          <v-col class="text-center" cols="2">
-            <v-btn @click="action2" icon>
-              <v-icon>mdi-magnify</v-icon>
-            </v-btn>
-          </v-col>
-          <v-col class="text-center" cols="2">
-            <v-btn @click="action3" icon>
-              <v-icon>mdi-plus-circle</v-icon>
-            </v-btn>
-          </v-col>
-          <v-col class="text-center" cols="2">
-            <v-btn @click="action4" icon>
-              <v-icon>mdi-heart</v-icon>
-            </v-btn>
-          </v-col>
-          <v-col class="text-center" cols="2">
-            <v-btn @click="action5" icon>
-              <v-icon>mdi-account</v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-bottom-navigation>
-
-      <!-- Exibe o conteúdo da rota -->
-      <router-view :style="{ 'background-image': 'url(' + $assetsBucket + '/backgrounds/backgrounds.png' +')' }"/>
+    <!-- Exibe o conteúdo da rota -->
+    <router-view :style="{ 'background-image': 'url(' + $assetsBucket + '/backgrounds/backgrounds.png' + ')' }" />
 
     <!-- Footer Section -->
     <v-footer class="footer black bg-black pb-12" padless>
       <v-row justify="center" align="center" class="text-center">
         <v-col cols="12" sm="4">
-          <v-img
-            class="logocgs mx-auto"
-            src="@/assets/cgs.png"
-            max-width="92"
-            alt="logo"
-          />
+          <v-img class="logocgs mx-auto" src="@/assets/cgs.png" max-width="92" alt="logo" />
         </v-col>
 
         <v-col cols="12" sm="4" class="info-footer text-center">
@@ -155,6 +103,13 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { useRouter } from 'vue-router';
+import Dashboard from "./pages/Dashboard.vue";
+
+const router = useRouter();
+
+router.push({ name: "" })
+
 
 const theme = ref("myCustomTheme");
 // Controle de visibilidade do menu de navegação inferior
@@ -177,10 +132,10 @@ const drawer = ref(false); // Controle do drawer lateral
 // Itens do menu de navegação
 const menuItems = ref([
   { title: "Dashboard", icon: "mdi-view-dashboard", to: { name: "Dashboard" } },
-  { title: "Campaign", icon: "mdi-flag", to: { name: "Campaign" } }, 
+  { title: "Campaign Tracker", icon: "mdi-flag", to: { name: "CampaignTracker" } },
   { title: "Library", icon: "mdi-book", to: { name: "Library" } },
-  { title: "Profile", icon: "mdi-account", to: { name: "Profile" } }, 
-  { title: "Events", icon: "mdi-calendar", to: { name: "Events" } }, 
+  { title: "Profile", icon: "mdi-account", to: { name: "Perfil" } },
+  { title: "Events", icon: "mdi-calendar", to: { name: "Events" } },
 ]);
 
 // Função de navegação
@@ -196,7 +151,7 @@ const closeDialog = () => {
 // Métodos de ação para os botões da navegação
 const action1 = () => {
   console.log("Home button clicked");
-  // Adicione a lógica específica para essa ação
+  router.push;
 };
 
 const action2 = () => {
@@ -225,4 +180,3 @@ const action5 = () => {
   width: 100%;
 }
 </style>
-
