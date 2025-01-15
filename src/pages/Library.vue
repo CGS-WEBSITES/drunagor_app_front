c850f947e3c620d39b3fa1bf40fddd0fd366b321
dudu1337 <54040715+dudu1337@users.noreply.github.com>
2025-01-15 17:36:13 -0300

    Reapply "removing fitlers"
    This reverts commit a793422627439af07f22df6ff51b17cc053c552d.

--- src/pages/Library.vue
+++ src/pages/Library.vue
@@ -16,151 +16,141 @@
 
       <!-- Filtros -->
       <v-col cols="12" md="3" v-show="showFilters || isDesktop">
-        <Filters
-          v-model:filterStatus="filterStatus"
-          v-model:rewardsStatus="rewardsStatus"
-          v-model:selectedBox="selectedBox"
-          v-model:componentChecked="componentChecked"
-          v-model:selectedComponentType="selectedComponentType"
-          v-model:contentChecked="contentChecked"
-          v-model:selectedContent="selectedContent"
-          :box-options="boxOptions"
-          :content-options="contentOptions"
-          :component-types="componentTypes"
-          @apply-filters="applyFilters"
-        />
+        <Filters @apply-filters="applyFilters" />
       </v-col>
 
       <!-- Galeria de Produtos -->
       <v-col cols="12" md="9">
         <v-row justify="center" align="center" dense>
-          <v-col
-            cols="12"
-            sm="6"
-            md="6"
-            class="d-flex justify-center"
-            v-for="product in products"
-            :key="product.id"
-          >
+          <v-col cols="12" sm="6" md="6" class="d-flex justify-center" v-for="product in products" :key="product.id">
             <!-- Componente de Card -->
-            <ProductCard
-              :product="product"
-              class="w-100"
-              @click="openLink(product.link)"
-            />
+            <ProductCard :product="product" class="w-100" @click="setDialog(product.name)" />
           </v-col>
         </v-row>
       </v-col>
     </v-row>
+    <v-dialog v-model="dialog" max-width="500">
+      <v-card>
+        <v-card-title>
+          {{ cardName }}
+        </v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn text="Close Dialog" @click="dialog = false"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, computed } from "vue";
+<script lang="ts" setup>
+import { ref, computed, onBeforeMount, inject } from "vue";
 import Filters from "@/components/Library/Filters.vue";
 import ProductCard from "@/components/ProductCard.vue";
+import { useUserStore } from "@/store/UserStore";
 
-export default defineComponent({
-  name: "Library",
-  components: { Filters, ProductCard },
-  setup() {
-    const filterStatus = ref("owned");
-    const rewardsStatus = ref("rewards_owned");
-    const selectedBox = ref("");
-    const componentChecked = ref(false);
-    const selectedComponentType = ref("");
-    const contentChecked = ref(false);
-    const selectedContent = ref("");
+const filterStatus = ref("owned");
+const rewardsStatus = ref("rewards_owned");
+const selectedBox = ref("");
+const componentChecked = ref(false);
+const selectedComponentType = ref("");
+const contentChecked = ref(false);
+const selectedContent = ref("");
+const user = useUserStore().user
+const cardName = ref("")
 
-    const boxOptions = ["Companions and Furnitures", "AoDarkness", "Desert of Hellscar"];
-    const contentOptions = ["Core", "Cosmetic", "Game Content"];
-    const componentTypes = ["Books", "Cards", "Miniatures", "Maps", "Doors", "Playerboards", "Punchboards", "Scorepad", "Trays"];
+const boxOptions = ["Companions and Furnitures", "AoDarkness", "Desert of Hellscar"];
+const contentOptions = ["Core", "Cosmetic", "Game Content"];
+const componentTypes = ["Books", "Cards", "Miniatures", "Maps", "Doors", "Playerboards", "Punchboards", "Scorepad", "Trays"];
 
-    const showFilters = ref(false);
-    const isDesktop = computed(() => window.innerWidth >= 960);
+const showFilters = ref(false);
+const isDesktop = computed(() => window.innerWidth >= 960);
+const setDialog = (name: string) => {
+  cardName.value = name
+  dialog.value = true
+}
 
-    const products = ref([
-      {
-        id: 1,
-        name: "Corebox",
-        image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-corebox.png",
-        link: "https://aodarkness.com/boxes/chronicles-of-drunagor-age-of-darkness-core-box/",
-      },
-      {
-        id: 2,
-        name: "Desert Of Hellscar",
-        image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-hellscar.png",
-        link: "https://aodarkness.com/boxes/desert-of-hellscar/",
-      },
-      {
-        id: 3,
-        name: "Lordwrath",
-        image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-handuriel.png",
-        link: "https://aodarkness.com/boxes/lordwrath/",
-      },
-      {
-        id: 4,
-        name: "Monster Pack",
-        image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-lordwrath.png",
-        link: "https://aodarkness.com/boxes/monster-pack/",
-      },
-      {
-        id: 5,
-        name: "Ruin of Luccanor",
-        image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-monsterpack.png",
-        link: "https://aodarkness.com/boxes/ruin-of-luccanor/",
-      },
-      {
-        id: 6,
-        name: "Shadow World",
-        image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-shadowworld.png",
-        link: "https://aodarkness.com/boxes/shadow-world/",
-      },
-      {
-        id: 7,
-        name: "Spoils of War",
-        image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-spoils.png",
-        link: "https://aodarkness.com/boxes/spoils-of-war/",
-      },
-      {
-        id: 8,
-        name: "Undead Dragon",
-        image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-undeaddragon.png",
-        link: "https://aodarkness.com/boxes/undead-dragon/",
-      },
-    
-    ]);
+const dialog = ref(true)
 
-    const toggleFilters = () => {
-      showFilters.value = !showFilters.value;
-    };
-
-    const openLink = (link: string) => {
-      window.open(link, "_blank");
-    };
-
-    const applyFilters = () => {
-      // Aqui pode-se implementar lógica de filtros para ajustar os produtos exibidos
-    };
-
-    return {
-      filterStatus,
-      rewardsStatus,
-      selectedBox,
-      componentChecked,
-      selectedComponentType,
-      contentChecked,
-      selectedContent,
-      boxOptions,
-      contentOptions,
-      componentTypes,
-      showFilters,
-      isDesktop,
-      products,
-      toggleFilters,
-      applyFilters,
-      openLink,
-    };
+const products = ref([
+  {
+    id: 1,
+    name: "Corebox",
+    image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-corebox.png",
+    link: "https://aodarkness.com/boxes/chronicles-of-drunagor-age-of-darkness-core-box/",
   },
-});
+  {
+    id: 2,
+    name: "Desert Of Hellscar",
+    image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-hellscar.png",
+    link: "https://aodarkness.com/boxes/desert-of-hellscar/",
+  },
+  {
+    id: 3,
+    name: "Lordwrath",
+    image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-handuriel.png",
+    link: "https://aodarkness.com/boxes/lordwrath/",
+  },
+  {
+    id: 4,
+    name: "Monster Pack",
+    image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-lordwrath.png",
+    link: "https://aodarkness.com/boxes/monster-pack/",
+  },
+  {
+    id: 5,
+    name: "Ruin of Luccanor",
+    image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-monsterpack.png",
+    link: "https://aodarkness.com/boxes/ruin-of-luccanor/",
+  },
+  {
+    id: 6,
+    name: "Shadow World",
+    image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-shadowworld.png",
+    link: "https://aodarkness.com/boxes/shadow-world/",
+  },
+  {
+    id: 7,
+    name: "Spoils of War",
+    image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-spoils.png",
+    link: "https://aodarkness.com/boxes/spoils-of-war/",
+  },
+  {
+    id: 8,
+    name: "Undead Dragon",
+    image: "https://druna-assets.s3.us-east-2.amazonaws.com/Library/box-undeaddragon.png",
+    link: "https://aodarkness.com/boxes/undead-dragon/",
+  },
+
+]);
+
+const toggleFilters = () => {
+  showFilters.value = !showFilters.value;
+};
+
+const openLink = (link: string) => {
+  window.open(link, "_blank");
+};
+
+const applyFilters = () => {
+  // Aqui pode-se implementar lógica de filtros para ajustar os produtos exibidos
+};
+
+const axios: any = inject("axios");
+const url: string = inject("apiUrl");
+
+onBeforeMount(async () => {
+  await axios
+    .get(url + "skus/search", {
+      limit: 30,
+    })
+    .then((response: any) => {
+      console.log("API Response:", response);
+    })
+    .catch((error: any) => {
+      console.log("API Response:", error);
+    });
+})
+
 </script>
