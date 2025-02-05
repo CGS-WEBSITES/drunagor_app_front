d4cc99a45bcae0db3496ed54cff71defe25db3d3
Luís Eduardo <54040715+dudu1337@users.noreply.github.com>
2025-02-05 11:40:57 -0300

    Adjusting Profile Card

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,24 +1,82 @@
 <template>
-  <div>
-    <a :href="product.link" target="_blank" rel="noopener noreferrer">
-      <div class="product-card">
-        <!-- Substitua o conteúdo abaixo pelo layout do seu card -->
-        <h3>{{ product.name }}</h3>
-        <p>{{ product.description }}</p>
-      </div>
-    </a>
-  </div>
+   <v-card
+    class="profile-card mx-auto py-0"
+    rounded="0"
+    elevation="3"
+    style="
+      overflow: visible;
+      position: relative;
+      text-align: center;
+      width: 100%;
+    "
+  >
+    <div class="position-relative">
+      <v-img
+        src="@/assets/AGE_Arte271.png"
+        alt="Background Image"
+        max-height="729px"
+        max-width="100%"
+        cover
+      ></v-img>
+
+      <!-- <v-btn icon="mdi-pencil" class="position-absolute top-0 right-0 ma-2" color="rgba(0, 0, 0, 0.6)" elevation="3"
+        :to="'/perfil/perfil-image'"></v-btn> -->
+    </div>
+
+    <v-img
+      :key="reloadKey"
+      :src="
+        user.picture_hash
+          ? assets + '/Profile/' + user.picture_hash
+          : assets + '/Profile/user.png'
+      "
+      :alt="user.picture_hash"
+      max-width="118"
+      style="
+        top: -30px;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        border: 0.5px solid white;
+        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+        background-color: black;
+      "
+    >
+      <v-btn
+        icon
+        class="position-absolute bottom-0 right-0 ma-1"
+        color="rgba(0, 0, 0, 0.6)"
+        elevation="3"
+        rounded="xl"
+        size="x-small"
+      >
+        <v-icon>mdi-pencil</v-icon>
+        <profile-pic-dialog></profile-pic-dialog>
+      </v-btn>
+    </v-img>
+
+    <v-card-text>
+      <div class="user-info" style="margin-top: -80px">
+        <p class="user-name" style="font-weight: bold; font-size: 1.4rem">
+          {{ user.user_name }}
+        </p>
+        <!-- <p class="user-points" style="font-size: 1.1rem;">
+          {{ points }} pts
+        </p>
+        <p class="user-ranking" style="font-size: 1.1rem;">
+          {{ ranking }}°
+        </p> -->
+        </div>
+      </v-card-text>
+    </v-card>
 </template>
 
-<script>
-export default {
-  props: {
-    product: {
-      type: Object,
-      required: true,
-    },
-  },
-};
+<script lang="ts" setup>
+import { inject, computed } from "vue";
+import { useUserStore } from "@/store/UserStore";
+
+const reloadKey = ref(0);
+const user = computed(() => useUserStore().user); // Inicializa a store
+const assets = inject<string>("assets");
 </script>
 
 <style scoped>
