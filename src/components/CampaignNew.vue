0ebf0c21b4a560521a53d146eefa8b4e5472df42
leandro-miranda <leandromiranda2010@gmail.com>
2025-02-22 12:13:54 -0300

    Replace random campaign ID generation with fixed IDs - Remove nanoid-based ID generation. - Introduce a mapping for fixed campaign IDs for "core", "apocalypse", and "awakenings". - Update newCampaign function to use the fixed IDs.

--- src/components/CampaignNew.vue
+++ src/components/CampaignNew.vue
@@ -5,28 +5,41 @@ import ApocalypseLogo from "@/assets/campaign/logo/apocalypse.webp";
 import AwakeningsLogo from "@/assets/campaign/logo/awakenings.webp";
 import { CampaignStore } from "@/store/CampaignStore";
 import { Campaign } from "@/store/Campaign";
-import { customAlphabet } from "nanoid";
 import { useRouter } from "vue-router";
 import { useI18n } from "vue-i18n";
 
 const visible = ref(false);
 const campaignStore = CampaignStore();
-const nanoid = customAlphabet("1234567890", 5);
 const router = useRouter();
 const { t } = useI18n();
 
+// Defina os IDs fixos para cada campanha
+const campaignIds: Record<"core" | "apocalypse" | "awakenings", string> = {
+  core: "001",
+  apocalypse: "002",
+  awakenings: "003"
+};
+
+const appUserString = localStorage.getItem('app_user')
+if (appUserString) {
+  const appUser = JSON.parse(appUserString);
+  const usersPk = appUser.users_pk;
+  console.log('usersPk', usersPk)
+}
+
 function newCampaign(campaign: "core" | "apocalypse" | "awakenings") {
-  let campaignId = nanoid();
+  let campaignId = campaignIds[campaign];
   campaignStore.add(new Campaign(campaignId, campaign));
   visible.value = false;
+  console.log('usersPk', appUserString)
   router.push("/campaign-tracker/campaign/" + campaignId);
 }
 </script>
 
 <template>
-  <v-btn variant="elevated" id="campaign-new" rounded @click="visible = true">{{
-    t("label.new-campaign")
-  }}</v-btn>
+  <v-btn variant="elevated" id="campaign-new" rounded @click="visible = true">
+    {{ t("label.new-campaign") }}
+  </v-btn>
   <v-dialog v-model="visible">
     <v-card>
       <v-card-title class="text-center">
