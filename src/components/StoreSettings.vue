378beabf32f22406c35ce306f2bd0ab35c80a779
leandro-miranda <leandromiranda2010@gmail.com>
2025-03-11 17:04:32 -0300

    Formatting

--- src/components/StoreSettings.vue
+++ src/components/StoreSettings.vue
@@ -1,48 +1,42 @@
 <template>
-   
-    <v-container>
-  
-    <StoreForm/>
-  
-      <v-row>
-        <v-col class="pa-0 ml-" cols="12" v-for="(store, index) in storeForms" :key="index">
-          <StoreForm v-model="storeForms[index]" @remove="removeStore(index)" />
-        </v-col>
-      </v-row>
-    </v-container>
-  
+  <v-container>
+    <StoreForm />
 
-  </template>
-  
-  
-  <script lang="ts" setup>
-  import { ref, computed } from "vue";
-  import { useUserStore } from "@/store/UserStore";
-  import StoreForm from "@/components/StoreForm.vue"; // Importa o componente do formulário
-  
-  const user = computed(() => useUserStore().user); // Obtém os dados do usuário
-  
-  const storeForms = ref([]); // Lista de formulários de loja
-  
-  // Adiciona um novo formulário de loja
-  const addStoreForm = () => {
-    storeForms.value.push({
-      name: "",
-      location: "",
-      description: "",
-    });
-  };
-  
-  // Remove um formulário de loja específico
-  const removeStore = (index) => {
-    storeForms.value.splice(index, 1);
-  };
-  
-  
-  
-  
-  </script>
-  
-  <style scoped>
-  </style>
-  
+    <v-row>
+      <v-col
+        class="pa-0 ml-"
+        cols="12"
+        v-for="(store, index) in storeForms"
+        :key="index"
+      >
+        <StoreForm v-model="storeForms[index]" @remove="removeStore(index)" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from "vue";
+import { useUserStore } from "@/store/UserStore";
+import StoreForm from "@/components/StoreForm.vue"; // Importa o componente do formulário
+
+const user = computed(() => useUserStore().user); // Obtém os dados do usuário
+
+const storeForms = ref([]); // Lista de formulários de loja
+
+// Adiciona um novo formulário de loja
+const addStoreForm = () => {
+  storeForms.value.push({
+    name: "",
+    location: "",
+    description: "",
+  });
+};
+
+// Remove um formulário de loja específico
+const removeStore = (index) => {
+  storeForms.value.splice(index, 1);
+};
+</script>
+
+<style scoped></style>
