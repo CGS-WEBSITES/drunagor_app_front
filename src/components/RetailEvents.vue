8fb3bdb8e5f4c9f3416befbd6a0d890b028744a1
Dudu Heylens <54040715+dudu1337@users.noreply.github.com>
2025-04-30 16:49:22 -0300

    feat(retailer_events): remove function to join events for retailer

--- src/components/RetailEvents.vue
+++ src/components/RetailEvents.vue
@@ -131,12 +131,6 @@
               </v-row>
             </v-card-text>
             <v-row class="mt-2 ml-0">
-              <v-col cols="6" class="pa-0">
-                <v-btn block color="#907041" class="rounded-0" @click="joinEvent">Maybe I’ll Go</v-btn>
-              </v-col>
-              <v-col cols="6" class="pa-0">
-                <v-btn block color="#539041" class="rounded-0" @click="joinEvent">Count me in</v-btn>
-              </v-col>
             </v-row>
           </v-card>
         </v-dialog>
