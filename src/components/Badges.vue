3d1902307437daceb2fa56656d159507b91225aa
Dudu Heylens <54040715+dudu1337@users.noreply.github.com>
2025-05-06 18:52:26 -0300

    feat(profile): mensage when no have rewards

--- src/components/Badges.vue
+++ src/components/Badges.vue
@@ -4,39 +4,51 @@
       <!-- Cabeçalho -->
       <v-card-title class="d-flex justify-space-between pb-0">
         <span class="text-uppercase font-weight-black text-bold text-h5 mb-4 pb-0">BADGES</span>
-        <span class="text-uppercase text-bold text-h5 mb-4 pb-0">{{ userRewards.length }}</span>
       </v-card-title>
 
       <!-- Virtual Scroller -->
-      <v-virtual-scroll class="pt-0" :items="userRewards" :item-height="130">
-        <template #default="{ item }">
-          <v-card
-            rounded="lg"
-            elevation="3"
-            class="py-2 px-0 mb-3 d-flex align-center"
-            color="secundary"
-          >
-            <v-row class="align-center">
-              <!-- Ícone -->
-              <v-col cols="2" md="2" class="d-flex align-center justify-center ml-3 ml-md-0">
-                <v-img
-                  :src="item.image"
-                  alt="Reward Icon"
-                  max-height="80"
-                  class="rounded-lg"
-                ></v-img>
-              </v-col>
+      <div v-if="userRewards.length > 0">
+  <v-virtual-scroll class="pt-0" :items="userRewards" :item-height="130">
+    <template #default="{ item }">
+      <v-card
+        rounded="lg"
+        elevation="3"
+        class="py-2 px-0 mb-2 d-flex align-center"
+        color="secundary"
+      >
+        <v-row class="align-center">
+          <!-- Ícone -->
+          <v-col cols="2" lg="2" class="d-flex align-center justify-center ml-3 ml-md-0">
+            <v-img
+              :src="item.image"
+              alt="Reward Icon"
+              max-height="80"
+              class="rounded-lg"
+            ></v-img>
+          </v-col>
 
-              <!-- Detalhes -->
-              <v-col cols="9" class="pl-0 d-flex flex-column justify-center">
-                <p class="font-weight-bold white--text ma-0">{{ item.title }}</p>
-                <p class="text-body-2 grey--text ma-0">{{ item.description }}</p>
-                <p class="text-caption grey--text ma-0">{{ item.date }}</p>
-              </v-col>
-            </v-row>
-          </v-card>
-        </template>
-      </v-virtual-scroll>
+          <!-- Detalhes -->
+          <v-col cols="9" class="pl-0 d-flex flex-column justify-center">
+            <p class="font-weight-bold white--text ma-0">{{ item.title }}</p>
+            <p class="text-body-2 grey--text ma-0">{{ item.description }}</p>
+            <p class="text-caption grey--text ma-0">{{ item.date }}</p>
+          </v-col>
+        </v-row>
+      </v-card>
+    </template>
+  </v-virtual-scroll>
+</div>
+
+<!-- Mensagem de nenhum reward -->
+<div v-else class="text-center py-2">
+  <v-icon color="grey lighten-1" size="48">mdi-emoticon-sad-outline</v-icon>
+  <p class="mt-4 text-body-1 white--text font-weight-medium">
+    You don't have any rewards yet.
+  </p>
+  <p class="text-caption grey--text">
+    Join events to fight for them!
+  </p>
+</div>
     </v-card>
   </v-container>
 </template>
