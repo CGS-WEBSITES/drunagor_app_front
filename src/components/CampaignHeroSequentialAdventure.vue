ce80b4904fee0fab486e4316c61c191dd034f0ac
Esterci <thiago.esterci.fernandes@gmail.com>
2025-05-28 18:41:05 -0300

    fix(vuetify): Vuetify lab compoent imports

--- src/components/CampaignHeroSequentialAdventure.vue
+++ src/components/CampaignHeroSequentialAdventure.vue
@@ -6,7 +6,6 @@ import { HeroDataRepository } from "@/data/repository/HeroDataRepository";
 import { useRoute } from "vue-router";
 import type { HeroData } from "@/data/repository/HeroData";
 import { useI18n } from "vue-i18n";
-import { VNumberInput } from "vuetify/labs/VNumberInput";
 
 const resourceDefinitions = [
   { id: "focus", translation_key: "label.focus" },
