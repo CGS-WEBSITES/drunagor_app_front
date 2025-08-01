789d6258d5e3b0252be6a100e39174025e76760c
Leandro Miranda <leandromiranda2010@gmail.com>
2025-08-01 16:24:04 -0300

    feat(campaign-player-list): replace static list with Vuetify expansion panel

--- src/components/CampaignPlayerList.vue
+++ src/components/CampaignPlayerList.vue
@@ -1,75 +1,86 @@
 <template>
-  <v-card color="primary" rounded="lg" elevation="0" class="py-2">
-    <h3 class="text-h6 mb-3">Players</h3>
-
-    <v-row v-if="players.length > 0" dense>
-      <v-col
-        v-for="item in players"
-        :key="item.rl_campaigns_users_pk"
-        cols="12"
-        sm="12"
+  <v-expansion-panels accordion>
+    <v-expansion-panel>
+      <v-expansion-panel-title
+        class="d-flex align-center justify-space-between"
       >
-        <v-card
-          class="pa-1 cursor-pointer"
-          style="position: relative"
-          rounded="lg"
-          elevation="6"
-          height="82px"
-          @click="navigateToUser(item.users_fk)"
-        >
-          <div
-            class="background-overlay"
-            :style="{
-              backgroundImage: item.background_hash
-                ? `url(https://druna-assets.s3.us-east-2.amazonaws.com/Profile/${item.background_hash})`
-                : 'url(https://druna-assets.s3.us-east-2.amazonaws.com/Profile/profile-bg-warriors-transparent.png)',
-            }"
-          ></div>
-
-          <v-row
-            align="center"
-            no-gutters
-            class="fill-height"
-            style="position: relative; z-index: 1"
+        <span class="text-h6">Players</span>
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-row v-if="players.length > 0" dense>
+          <v-col
+            v-for="item in players"
+            :key="item.rl_campaigns_users_pk"
+            cols="12"
+            sm="12"
           >
-            <v-col cols="4" sm="2" class="d-flex justify-center align-center">
-              <v-img
-                :src="item.image"
-                alt="Player Image"
-                width="70"
-                height="77"
-                class="rounded-lg"
-              ></v-img>
-            </v-col>
+            <v-card
+              class="pa-1 cursor-pointer"
+              style="position: relative"
+              rounded="lg"
+              elevation="6"
+              height="82px"
+              @click="navigateToUser(item.users_fk)"
+            >
+              <div
+                class="background-overlay"
+                :style="{
+                  backgroundImage: item.background_hash
+                    ? `url(https://druna-assets.s3.us-east-2.amazonaws.com/Profile/${item.background_hash})`
+                    : 'url(https://druna-assets.s3.us-east-2.amazonaws.com/Profile/profile-bg-warriors-transparent.png)',
+                }"
+              ></div>
 
-            <v-col cols="8" sm="9" class="pl-2">
-              <div class="d-flex align-center">
-                <v-icon
-                  v-if="item.role_name === 'Drunagor Master'"
-                  size="small"
-                  color="amber"
-                  class="mr-1"
+              <v-row
+                align="center"
+                no-gutters
+                class="fill-height"
+                style="position: relative; z-index: 1"
+              >
+                <v-col
+                  cols="4"
+                  sm="2"
+                  class="d-flex justify-center align-center"
                 >
-                  mdi-crown
-                </v-icon>
-                <p class="font-weight-bold text-body-1 text-truncate">
-                  {{ item.user_name }}
-                </p>
-              </div>
+                  <v-img
+                    :src="item.image"
+                    alt="Player Image"
+                    width="70"
+                    height="77"
+                    class="rounded-lg"
+                  ></v-img>
+                </v-col>
 
-              <div class="text-caption text-grey-lighten-1">
-                {{ item.role_name }}
-              </div>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
-    </v-row>
+                <v-col cols="8" sm="9" class="pl-2">
+                  <div class="d-flex align-center">
+                    <v-icon
+                      v-if="item.role_name === 'Drunagor Master'"
+                      size="small"
+                      color="amber"
+                      class="mr-1"
+                    >
+                      mdi-crown
+                    </v-icon>
+                    <p class="font-weight-bold text-body-1 text-truncate">
+                      {{ item.user_name }}
+                    </p>
+                  </div>
 
-    <div v-else class="text-center pa-4 text-grey">
-      <p>No other players found in this campaign.</p>
-    </div>
-  </v-card>
+                  <div class="text-caption text-grey-lighten-1">
+                    {{ item.role_name }}
+                  </div>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <div v-else class="text-center pa-4 text-grey">
+          <p>No other players found in this campaign.</p>
+        </div>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script setup lang="ts">
@@ -85,7 +96,6 @@ const router = useRouter();
 const players = ref<any[]>([]);
 
 const navigateToUser = (userId: number) => {
-  // Lembrete: a navegação depende do 'users_fk' ser retornado pela API.
   if (!userId) return;
   const encodedId = btoa(userId.toString());
   router.push({ name: "User", params: { id: encodedId } });
