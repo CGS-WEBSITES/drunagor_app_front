a6376f4a1c43bca180b9248c62b19e80a645b0b6
Dudu Heylens <54040715+dudu1337@users.noreply.github.com>
2025-09-29 11:15:37 -0300

    feat(companion): set to hide companion title when in camapgin

--- src/pages/CampaignTracker.vue
+++ src/pages/CampaignTracker.vue
@@ -1,23 +1,29 @@
 <template>
   <v-container fluid class="pa-0" :style="contentStyle">
-    <v-row no-gutters class=" cinzel-text font-weight-black pt-15 pb-4 justify-center text-center text-h2">
-      COMPANION</v-row
+    <v-row
+      v-if="!isCampaignRoute"
+      no-gutters
+      class="cinzel-text font-weight-black pt-15 pb-4 justify-center text-center text-h2"
     >
+      COMPANION
+    </v-row>
     <MainMenu />
-        <router-view />
-
+    <router-view />
   </v-container>
 </template>
 
-<script>
+<script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
 import MainMenu from "@/components/MainMenu.vue";
+
+const route = useRoute();
+
+const isCampaignRoute = computed(() => route.name === "Campaign");
 </script>
 
 <style>
 .cinzel-text {
-  font-family: 'Cinzel', serif;
+  font-family: "Cinzel", serif;
 }
-
-
-
 </style>
