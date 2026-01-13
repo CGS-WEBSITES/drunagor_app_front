a297713214b3087c6a8604e73c66821591be3276
Dudu Heylens <54040715+dudu1337@users.noreply.github.com>
2026-01-13 17:35:48 -0300

    feat(tutorial): import tutorial in app

--- src/App.vue
+++ src/App.vue
@@ -159,6 +159,7 @@ import { setToken } from "@/service/AccessToken";
 import { useRouter, useRoute } from "vue-router";
 import { useDisplay } from "vuetify";
 import { useUserStore } from "@/store/UserStore";
+import { useTutorialStore } from "@/store/TutorialStore";
 import themeIcon from "@/assets/theme.png";
 import { socketKey } from "@/plugins/socket";
 
@@ -168,6 +169,7 @@ const openLink = (url) => {
 };
 
 const userStore = useUserStore();
+const tutorialStore = useTutorialStore();
 const user = computed(() => userStore.user);
 
 const display = ref(useDisplay());
@@ -307,6 +309,8 @@ onMounted(() => {
   if (userObject) {
     useUserStore().setUser(userObject);
   }
+
+   tutorialStore.loadPreferences();
 });
 
 onBeforeMount(() => {
