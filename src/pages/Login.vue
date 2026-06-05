18e96bea99f9f3318f0d5e0c2531e1419523c31e
Dudu Heylens <luiseduardoekenya7@gmail.com>
2026-06-05 11:07:53 -0300

    style(auth): align background image to top center for mobile viewports

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -467,7 +467,7 @@ onBeforeMount(() => {
 .login-page {
   background-image: url("https://assets.drunagor.app/backgrounds/login-background.png");
   background-size: cover;
-  background-position: center;
+  background-position: top center;
   background-repeat: no-repeat;
   min-height: 100vh;
 }
